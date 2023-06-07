5c780253246f778a43083640023bbc1c788d9a1c
Davi Batista <davideosmar11@gmail.com>
2023-06-07 11:37:36 -0300

    V1.6 | feat: added 'Reservations' Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import AboutUs from "./components/AbousUs/AboutUs"
 import Introduction from "./components/Introduction/Introduction"
+import ReservationsContainer from "./components/ReservationsContainer/ReservationsContainer"
 import RoomsContainer from "./components/RoomsContainer/RoomsContainer"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Introduction/>
       <AboutUs/>
       <RoomsContainer/>
+      <ReservationsContainer/>
     </>
   )
 }
--- src/components/ReservationsContainer/ReservationsContainer.tsx
+++ src/components/ReservationsContainer/ReservationsContainer.tsx
@@ -1,14 +1,31 @@
+import Separator from '../Separator/Separator';
 import styles from './styles.module.scss';
 
 function ReservationsContainer ()    {
     return(
         <>
             <section className={styles.section}>
-                <div>
-
+                <div className={styles.formDiv}>
+                    <h1 className={styles.titleForm}>Entre em contato e faça sua reserva</h1>
+                        <Separator bg={'#064635'} />
+                        <form className={styles.form}>
+                        <label className={styles.labels} htmlFor="name">Nome</label>
+                        <input className={styles.inputs} type="text" id='name' name='name'/>
+                        <label className={styles.labels} htmlFor="email">Email</label>
+                        <input className={styles.inputs} type="text" id='email' name='email'/>
+                        <span className={styles.span}>
+                            <input type="radio" id="hills" name="unit" value="Hills" className={styles.radios} />
+                            <label htmlFor="hills" className={styles.labels} >Hills</label>
+                            <input type="radio" id="shores" name="unit" value="Shores" className={styles.radios} />
+                            <label htmlFor="shores" className={styles.labels} >Shores</label>
+                        </span>
+                        <label className={styles.labels} htmlFor="message">Mensagem</label>
+                        <input type="text" id='message' name='message' className={styles.message} />
+                        <button className={styles.reservation}>Enviar</button>
+                        </form>
                 </div>
-                <div>
-
+                <div className={styles.imageDiv}>
+                    <img src="../src/images/hotel-lounge.jpg" alt="Hotel Lounge" className={styles.image} />
                 </div>
             </section>
         </>
